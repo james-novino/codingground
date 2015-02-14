83d9b25db2877314d63c2f7c86d4d8a1f6374744
James Novino <james-novino@users.noreply.github.com>
2015-02-14 16:11:13 -0500

    Created by Coding Ground Client

---
+++ New Project/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int amount;
+    printf("Enter Value: ");
+    scanf("%d",&amount);
+    int test = amount/20;
+    printf("\n%d\n",test);
+    main();
+}
